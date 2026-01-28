b82eb07d2ddaa0d2b8c235be0ecb7a491d5a3aab
Gabriel-MRodrigues <gabrielmendesrodriguest@hotmail.com>
2026-01-28 19:05:07 -0400

    feature: information list for about me section

--- assets/assets.js
+++ assets/assets.js
@@ -1,5 +1,27 @@
 import profile_image from './Profile-Picture.jpg';
+import { FaCode } from 'react-icons/fa';
+import { LuCircuitBoard } from 'react-icons/lu';
+import { FaUsers } from 'react-icons/fa';
 
 export const assets = {
   profile_image,
 };
+
+export const infoList = [
+  {
+    icon: <FaCode />,
+    title: 'Web Development',
+    description: 'Creating fullstack web applications with modern frameworks.',
+  },
+  {
+    icon: <LuCircuitBoard />,
+    title: 'Embedded Development',
+    description: 'Building embedded systems with microcontrollers.',
+  },
+  {
+    icon: <FaUsers />,
+    title: 'Collaboration',
+    description:
+      'Working closely with teams to bring ideas and projects to life.',
+  },
+];
