ae0e0b5d5a7f44566a561e53c074192edfadb9bf
Gabriel-MRodrigues <gabrielmendesrodriguest@hotmail.com>
2026-01-28 16:13:51 -0400

    feature: mobile side bar func

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { TfiAngleDoubleRight } from 'react-icons/tfi';
 import { LuMoonStar } from 'react-icons/lu';
 import { RiMenu3Line } from 'react-icons/ri';
 import { RxCross2 } from 'react-icons/rx';
 
 const Navbar = () => {
+  const sideMenuRef = useRef();
+
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = 'translateX(-16rem)';
+  };
+
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = 'translateX(16rem)';
+  };
+
   return (
     <>
       <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
@@ -31,7 +41,7 @@ const Navbar = () => {
         </ul>
 
         <div className="flex items-center gap-4">
-          <button>
+          <button className="cursor-pointer">
             <LuMoonStar size={28} />
           </button>
           <a
@@ -40,30 +50,49 @@ const Navbar = () => {
           >
             Contact <TfiAngleDoubleRight />
           </a>
-          <button className="block md:hidden ml-3">
+          <button
+            className="block md:hidden ml-3 cursor-pointer"
+            onClick={openMenu}
+          >
             <RiMenu3Line size={28} />
           </button>
         </div>
 
         {/* Mobile menu */}
-        <ul className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500">
-          <div className="absolute right-6 top-6">
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <div
+            className="absolute right-6 top-6 cursor-pointer"
+            onClick={closeMenu}
+          >
             <RxCross2 size={28} />
           </div>
           <li>
-            <a href="#top">Home</a>
+            <a href="#top" onClick={closeMenu}>
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a href="#about" onClick={closeMenu}>
+              About
+            </a>
           </li>
           <li>
-            <a href="#skills">Skills</a>
+            <a href="#skills" onClick={closeMenu}>
+              Skills
+            </a>
           </li>
           <li>
-            <a href="#project">Projects</a>
+            <a href="#project" onClick={closeMenu}>
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a href="#contact" onClick={closeMenu}>
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
