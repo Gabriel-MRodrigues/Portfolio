d54b45848acaf1fbbe2b1c128d0c4413bbda3221
Gabriel-MRodrigues <gabrielmendesrodriguest@hotmail.com>
2026-01-29 15:00:53 -0400

    feature: defining footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IoArrowUpCircle } from 'react-icons/io5';
+
+const Footer = () => {
+  return (
+    <footer className="mx-auto py-5 px-4 relative border-t border-black mt-12 pt-8 flex justify-between items-center">
+      <p className="text-sm text-muted-foreground">
+        &copy; {new Date().getFullYear()} Gabriel Rodrigues. All rights
+        reserved.
+      </p>
+      <a href="#top" className="p-2 rounded-full">
+        <IoArrowUpCircle size={40} />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
